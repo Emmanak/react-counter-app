b89001d57be8d5015ea89402f50d446e27fed218
emmanak <emmanuelaugustine@hotmail.com>
2020-02-11 03:29:12 -0500

    second Commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    //value: this.props.counter.value,
+    imageUrl: "https://picsum.photos/50",
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        {this.props.children}
+        <img src={this.state.imageUrl} />
+
+        <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
+          {String(this.formatCount())}
+        </span>
+
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m2"
+        >
+          Delete
+        </button>
+
+        {this.state.tags.length === 0 && "Pkease create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes +=
+      this.props.counter.value === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
